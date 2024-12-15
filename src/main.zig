d0289608b69d2a16cdbe27d06cc81913ef15a3bc
Ariel Benichou <videojr3000@gmail.com>
2024-12-15 21:18:19 +0200

    refactor: Shader.zig struct

--- src/main.zig
+++ src/main.zig
@@ -4,9 +4,11 @@ const glfw = @import("zglfw");
 const zopengl = @import("zopengl");
 const zstbi = @import("zstbi");
 const gl = zopengl.bindings;
+const Shader = @import("Shader.zig").Shader;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
     const allocator = gpa.allocator();
     defer std.debug.assert(gpa.deinit() == .ok);
 
@@ -54,20 +56,12 @@ pub fn main() !void {
     );
     _ = window.setFramebufferSizeCallback(framebufferSizeCallback);
 
-    const shader_vertex = try createShader(
-        shader_vertex_source,
-        gl.VERTEX_SHADER,
-        "VERTEX",
+    const shader = try Shader.init(
+        allocator,
+        "./src/shaders/default.vert.glsl",
+        "./src/shaders/default.frag.glsl",
     );
-    const shader_fragment = try createShader(
-        shader_fragment_source,
-        gl.FRAGMENT_SHADER,
-        "FRAGMENT",
-    );
-    const shader_program = try createProgram(
-        shader_vertex,
-        shader_fragment,
-    );
+    defer shader.deinit();
 
     // zig fmt: off
     const vertices = [_]gl.Float {
@@ -81,10 +75,12 @@ pub fn main() !void {
 
     var vao: gl.Uint = undefined;
     gl.genVertexArrays(1, &vao);
+    defer gl.deleteVertexArrays(1, &vao);
     gl.bindVertexArray(vao);
 
     var vbo: gl.Uint = undefined;
     gl.genBuffers(1, &vbo);
+    defer gl.deleteBuffers(1, &vbo);
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(
         gl.ARRAY_BUFFER,
@@ -98,10 +94,19 @@ pub fn main() !void {
         3,
         gl.FLOAT,
         gl.FALSE,
-        3 * @sizeOf(gl.Float),
+        6 * @sizeOf(gl.Float),
         @ptrFromInt(0),
     );
     gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(
+        1,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        6 * @sizeOf(gl.Float),
+        @ptrFromInt(3 * @sizeOf(gl.Float)),
+    );
+    gl.enableVertexAttribArray(1);
 
     var texture: gl.Uint = undefined;
     gl.genTextures(1, &texture);
@@ -128,87 +133,25 @@ pub fn main() !void {
 
     glfw.swapInterval(1); // WHY?
     while (!window.shouldClose()) {
-        glfw.pollEvents();
         processInput(window);
 
         gl.clearColor(0.2, 0.3, 0.3, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
 
-        gl.useProgram(shader_program);
+        shader.use();
+
         gl.bindVertexArray(vao);
         gl.drawArrays(gl.TRIANGLES, 0, 3);
 
         window.swapBuffers();
+        glfw.pollEvents();
     }
 }
+
 const initial_screen_size = .{
     .width = 800,
     .height = 600,
 };
-
-const shader_vertex_source = @embedFile("./shaders/default.vert.glsl");
-
-const shader_fragment_source = @embedFile("./shaders/default.frag.glsl");
-
-fn createShader(source: [:0]const u8, shader_type: gl.Enum, name: [:0]const u8) !gl.Uint {
-    const shader = gl.createShader(shader_type);
-    gl.shaderSource(
-        shader,
-        1,
-        @alignCast(@ptrCast(&source)),
-        null,
-    );
-    gl.compileShader(shader);
-
-    var success: gl.Int = undefined;
-    gl.getShaderiv(shader, gl.COMPILE_STATUS, &success);
-    var info_log: [512]u8 = undefined;
-    var log_size: gl.Int = 0;
-    gl.getShaderInfoLog(
-        shader,
-        512,
-        &log_size,
-        @constCast(&info_log),
-    );
-    const i: usize = @intCast(log_size);
-    if (success == 0) {
-        std.debug.print("[OPENGL] ERROR::SHADER::{s}::COMPILATION_FAILED\n{s}\n", .{ name, info_log[0..i] });
-        return error.LinkingFailed;
-    } else {
-        std.debug.print("[OPENGL] INFO::SHADER::{s}::COMPILATION_SUCCESS\n{s}\n", .{ name, info_log[0..i] });
-    }
-
-    return shader;
-}
-
-fn createProgram(shader_vertex: gl.Uint, shader_fragment: gl.Uint) !gl.Uint {
-    const program = gl.createProgram();
-    gl.attachShader(program, shader_vertex);
-    gl.attachShader(program, shader_fragment);
-    gl.linkProgram(program);
-    gl.deleteShader(shader_vertex);
-    gl.deleteShader(shader_fragment);
-
-    var success: gl.Int = undefined;
-    gl.getProgramiv(program, gl.LINK_STATUS, &success);
-    var info_log: [512]u8 = undefined;
-    var log_size: gl.Int = 0;
-    gl.getProgramInfoLog(
-        program,
-        512,
-        &log_size,
-        @constCast(&info_log),
-    );
-    const i: usize = @intCast(log_size);
-    if (success == 0) {
-        std.debug.print("[OPENGL] ERROR::SHADER::PROGRAM::LINKING_FAILED\n{s}\n", .{info_log[0..i]});
-        return error.CompilationFailed;
-    } else {
-        std.debug.print("[OPENGL] INFO::SHADER::PROGRAM::LINKING_SUCCESS\n{s}\n", .{info_log[0..i]});
-    }
-
-    return program;
-}
 
 fn framebufferSizeCallback(window: *glfw.Window, width: i32, height: i32) callconv(.c) void {
     _ = window;
--- src/shaders/default.frag.glsl
+++ src/shaders/default.frag.glsl
@@ -2,6 +2,8 @@
 
 out vec4 frag_color;
 
+in vec3 vertex_color;
+
 void main() {
-  frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+  frag_color = vec4(vertex_color, 1.0);
 }
